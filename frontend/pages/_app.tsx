422dbdeaf0c21776e75c667edd29525143d9cbc1
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-03-30 16:57:13 +0600

    feat: add graphql client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,12 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import createApolloClient from '@/api/graphql/client';
-import { ApolloProvider } from '@apollo/client';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import createApolloClient from "@/api/graphql/client";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = createApolloClient();
   return (
-    <div className={'min-h-screen flex flex-col m-4'}>
+    <div className={"min-h-screen flex flex-col m-4"}>
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
