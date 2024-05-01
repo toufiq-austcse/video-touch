89e200d189454ff0bedbd1ba849c0e2fb063ee5a
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-05-01 18:25:06 +0600

    feat: add aws cli in docker compose

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,44 +1,44 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { ColumnDef } from '@tanstack/react-table';
-import { ChevronDown, MoreHorizontal } from 'lucide-react';
+import * as React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { ChevronDown, MoreHorizontal } from "lucide-react";
 
-import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
-import { badgeVariants } from '@/components/ui/badge';
-import Image from 'next/image';
-import Link from 'next/link';
-import AppTable from '@/components/ui/app-table';
-import { useQuery } from '@apollo/client';
-import { LIST_ASSETS } from '@/api/graphql/queries/query';
-import UploadNew from '@/components/ui/upload-new';
-import { VIDEO_STATUS } from '@/lib/constant';
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { badgeVariants } from "@/components/ui/badge";
+import Image from "next/image";
+import Link from "next/link";
+import AppTable from "@/components/ui/app-table";
+import { useQuery } from "@apollo/client";
+import { LIST_ASSETS } from "@/api/graphql/queries/query";
+import UploadNew from "@/components/ui/upload-new";
+import { VIDEO_STATUS } from "@/lib/constant";
 
 export type Video = {
   _id: string;
   title: string;
   thumbnail_url: string;
-  status: 'pending' | 'processing' | 'success' | 'failed';
+  status: "pending" | "processing" | "success" | "failed";
   created_at: Date;
 };
 
 export const columns: ColumnDef<Video>[] = [
   {
-    id: 'select',
+    id: "select",
     header: ({ table }) => (
       <Checkbox
         checked={
           table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && 'indeterminate')
+          (table.getIsSomePageRowsSelected() && "indeterminate")
         }
         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
@@ -52,12 +52,12 @@ export const columns: ColumnDef<Video>[] = [
       />
     ),
     enableSorting: false,
-    enableHiding: false
+    enableHiding: false,
   },
 
   {
-    accessorKey: 'title',
-    header: 'Title',
+    accessorKey: "title",
+    header: "Title",
     cell: ({ row }) => {
       return (
         <Link className="flex space-x-4" href={`/videos/${row.original._id}`}>
@@ -68,54 +68,54 @@ export const columns: ColumnDef<Video>[] = [
             height={100}
             priority
           />
-          <div className="lowercase font-medium">{row.getValue('title')}</div>
+          <div className="lowercase font-medium">{row.getValue("title")}</div>
         </Link>
       );
-    }
+    },
   },
   {
-    accessorKey: 'status',
-    header: 'Status',
+    accessorKey: "status",
+    header: "Status",
     cell: ({ row }) => {
-      let status = row.getValue('status');
+      let status = row.getValue("status");
       if (status === VIDEO_STATUS.FAILED) {
         return (
           <div
-            className={`${badgeVariants({ variant: 'destructive' })} capitalize`}
+            className={`${badgeVariants({ variant: "destructive" })} capitalize`}
           >
-            {row.getValue('status')}
+            {row.getValue("status")}
           </div>
         );
       } else if (status === VIDEO_STATUS.PROCESSING) {
         return (
           <div
-            className={`${badgeVariants({ variant: 'default' })} capitalize`}
+            className={`${badgeVariants({ variant: "default" })} capitalize`}
           >
-            {row.getValue('status')}
+            {row.getValue("status")}
           </div>
         );
       }
 
       return (
         <div
-          className={`${badgeVariants({ variant: 'secondary' })} capitalize`}
+          className={`${badgeVariants({ variant: "secondary" })} capitalize`}
         >
-          {row.getValue('status')}
+          {row.getValue("status")}
         </div>
       );
-    }
+    },
   },
   {
-    accessorKey: 'created_at',
-    header: 'CreatedAt',
+    accessorKey: "created_at",
+    header: "CreatedAt",
     cell: ({ row }) => {
-      const date = new Date(row.getValue('created_at')).toDateString();
+      const date = new Date(row.getValue("created_at")).toDateString();
 
       return <div>{date}</div>;
-    }
+    },
   },
   {
-    id: 'actions',
+    id: "actions",
     enableHiding: false,
     cell: ({ row }) => {
       const video = row.original;
@@ -139,8 +139,8 @@ export const columns: ColumnDef<Video>[] = [
           </DropdownMenuContent>
         </DropdownMenu>
       );
-    }
-  }
+    },
+  },
 ];
 
 export default function HomePage() {
@@ -150,16 +150,16 @@ export default function HomePage() {
   let { data, loading, error, fetchMore, refetch } = useQuery(LIST_ASSETS, {
     variables: {
       first: pageSize,
-      after: null
-    }
+      after: null,
+    },
   });
 
   const nextFunction = () => {
-    console.log('next');
+    console.log("next");
     fetchMore({
       variables: {
         first: pageSize,
-        after: data.ListAsset.page_info.next_cursor
+        after: data.ListAsset.page_info.next_cursor,
       },
       updateQuery: (prev, { fetchMoreResult }) => {
         setPageIndex((prev) => prev + 1);
@@ -167,16 +167,16 @@ export default function HomePage() {
           return prev;
         }
         return fetchMoreResult;
-      }
+      },
     });
   };
 
   const prevFunction = () => {
-    console.log('prev');
+    console.log("prev");
     fetchMore({
       variables: {
         first: pageSize,
-        before: data.ListAsset.page_info.prev_cursor
+        before: data.ListAsset.page_info.prev_cursor,
       },
       updateQuery: (prev, { fetchMoreResult }) => {
         setPageIndex((prev) => prev - 1);
@@ -184,7 +184,7 @@ export default function HomePage() {
           return prev;
         }
         return fetchMoreResult;
-      }
+      },
     });
   };
 
