f709f8a9039416c132566157672aeece6cb98b6a
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-05-25 19:58:24 +0600

    feat: seperate download worker

---
+++ server/workers/download-video-worker/src/common/rabbit-mq/rabbit-mq.module.ts
@@ -0,0 +1,23 @@
+import { Global, Module } from '@nestjs/common';
+import { RabbitMQModule as MQModule } from '@golevelup/nestjs-rabbitmq';
+import { RabbitMqService } from './service/rabbitmq.service';
+import { AppConfigService } from '@/src/common/app-config/service/app-config.service';
+
+@Global()
+@Module({
+  imports: [
+    MQModule.forRootAsync(MQModule, {
+      inject: [AppConfigService],
+      useFactory: () => {
+        console.log('RABBIT_MQ_URL ', AppConfigService.appConfig.RABBIT_MQ_URL);
+        return {
+          uri: AppConfigService.appConfig.RABBIT_MQ_URL,
+          connectionInitOptions: { wait: false },
+        };
+      },
+    }),
+  ],
+  providers: [RabbitMqService],
+  exports: [RabbitMqService],
+})
+export class RabbitMQModule {}
