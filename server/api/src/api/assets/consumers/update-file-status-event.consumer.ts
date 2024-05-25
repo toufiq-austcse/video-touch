f654d45174f3d05c85cce65d138679421bd8caf5
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-05-26 03:16:37 +0600

    feat: seperate download worker

--- server/api/src/api/assets/consumers/update-file-status-event.consumer.ts
+++ server/api/src/api/assets/consumers/update-file-status-event.consumer.ts
@@ -6,13 +6,12 @@ import { FileService } from '@/src/api/assets/services/file.service';
 
 @Injectable()
 export class UpdateFileStatusEventConsumer {
-  constructor(private fileService: FileService) {
-  }
+  constructor(private fileService: FileService) {}
 
   @RabbitSubscribe({
     exchange: process.env.RABBIT_MQ_VIDEO_TOUCH_TOPIC_EXCHANGE,
     routingKey: process.env.RABBIT_MQ_UPDATE_FILE_STATUS_ROUTING_KEY,
-    queue: process.env.RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE
+    queue: process.env.RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE,
   })
   public async handle(msg: UpdateFileStatusEventModel) {
     try {
