d85f12971d5f37c74ea085593d712f3bc45a5678
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-06-08 20:37:16 +0600

    feat: add duration

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -8,3 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function bytesToMegaBytes(bytes: number) {
   return (bytes / 1024 / 1024).toFixed(2);
 }
+
+export function secondsToHHMMSS(seconds: number) {
+  return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
