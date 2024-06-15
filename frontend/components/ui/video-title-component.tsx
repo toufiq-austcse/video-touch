b1f9c0eb9f0d897dda2287085b522d913aa0bef7
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-06-16 03:08:49 +0600

    feat: add title, description, tag edit

--- frontend/components/ui/video-title-component.tsx
+++ frontend/components/ui/video-title-component.tsx
@@ -1,15 +1,15 @@
-import { VideoDetails } from "@/api/graphql/types/video-details";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Check, X } from "lucide-react";
-import { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { Asset } from "@/api/graphql/graphql";
-import { UPDATE_ASSET_MUTATION } from "@/api/graphql/queries/query";
+import { VideoDetails } from '@/api/graphql/types/video-details';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Check, X } from 'lucide-react';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Asset } from '@/api/graphql/graphql';
+import { UPDATE_ASSET_MUTATION } from '@/api/graphql/queries/query';
 
 const VideoTitleComponent = ({
-  videoDetails,
-}: {
+                               videoDetails
+                             }: {
   videoDetails: VideoDetails;
 }) => {
   const [enableEditTitle, setEnableEditTitle] = useState(false);
@@ -26,8 +26,8 @@ const VideoTitleComponent = ({
     let res = await updateAsset({
       variables: {
         id: videoDetails._id,
-        title: tempTitle,
-      },
+        title: tempTitle
+      }
     });
     setTitle(tempTitle);
 
@@ -38,8 +38,7 @@ const VideoTitleComponent = ({
   };
 
   const onKeyDownInTitle = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log("key down in title");
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       onUpdateClick();
     }
   };
