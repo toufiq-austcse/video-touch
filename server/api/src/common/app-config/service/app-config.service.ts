4f028582a32c55929cf75f5f3c4bf84e549fb602
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2024-06-16 20:16:10 +0600

    feat: add cdn in master playlist url

--- server/api/src/common/app-config/service/app-config.service.ts
+++ server/api/src/common/app-config/service/app-config.service.ts
@@ -64,7 +64,7 @@ export class AppConfigService {
       ),
       RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE: this.configService.getOrThrow('RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE'),
       TEMP_VIDEO_DIRECTORY: this.configService.getOrThrow('TEMP_VIDEO_DIRECTORY'),
-      CDN_BASE_URL: this.configService.getOrThrow('CDN_BASE_URL')
+      CDN_BASE_URL: this.configService.getOrThrow('CDN_BASE_URL'),
     };
   }
 }
