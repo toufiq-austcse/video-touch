3ac67c036e5c36b7e28da8d92fb97bb18f0eceb7
Toufiq <toufiq.austcse@gmail.com>
2025-08-30 16:09:21 +0600

    chore: update apollo and graphql deps

--- server/api/src/api/api.module.ts
+++ server/api/src/api/api.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { IndexModule } from './index/index.module';
-import { AppConfigModule } from '@/src/common/app-config/app-config.module';
-import { HttpClientsModule } from '@/src/common/http-clients/http-clients.module';
-import { DatabaseModule } from '@/src/common/database/database.module';
 import { AssetsModule } from '@/src/api/assets/assets.module';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppConfigModule } from '@/src/common/app-config/app-config.module';
+import { AppConfigService } from '@/src/common/app-config/service/app-config.service';
+import { AwsModule } from '@/src/common/aws/aws.module';
+import { DatabaseModule } from '@/src/common/database/database.module';
+import { HttpClientsModule } from '@/src/common/http-clients/http-clients.module';
+import { RabbitMQModule } from '@/src/common/rabbit-mq/rabbit-mq.module';
+import { ExpressAdapter } from '@bull-board/express';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { ApolloDriver } from '@nestjs/apollo';
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AwsModule } from '@/src/common/aws/aws.module';
 import { AuthModule } from './auth/auth.module';
-import { BullModule } from '@nestjs/bullmq';
-import { RabbitMQModule } from '@/src/common/rabbit-mq/rabbit-mq.module';
-import { AppConfigService } from '@/src/common/app-config/service/app-config.service';
-import { BullBoardModule } from '@bull-board/nestjs';
-import { ExpressAdapter } from '@bull-board/express';
+import { IndexModule } from './index/index.module';
 import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
@@ -34,14 +34,14 @@ import { WebhookModule } from './webhook/webhook.module';
       route: '/queues',
       adapter: ExpressAdapter, // Or FastifyAdapter from `@bull-board/fastify`
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRoot<any>({
       cors: {
         origin: true,
         credentials: true,
       },
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true,
+      graphiql: true,
     }),
     HttpClientsModule,
     IndexModule,
